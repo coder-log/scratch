00c558d6fd7a3bd7f03bb6fcf090ab818ef6fb3f
Xavier <43478582+coder-log@users.noreply.github.com>
2019-02-19 08:32:21 +0100

    First files of the theme

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Front page
+ *
+ * @package     Scratch
+ * @author      Javier Sánchez Serrano
+ * @copyright   Copyright (c) 2019 Javier Sánchez Serrano
+ * @license     GPL-2.0+
+ *
+ */
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Theme functionality
+ *
+ * @package     Scratch
+ * @author      Javier Sánchez Serrano
+ * @copyright   Copyright (c) 2019 Javier Sánchez Serrano
+ * @license     GPL-2.0+
+ *
+ */
+
+ 
