f42ed36a7a00fb86ebfac55b5681bfdfcbce7722
Xavier <43478582+coder-log@users.noreply.github.com>
2019-02-20 05:58:23 +0100

    Finishin initial setup

--- front-page.php
+++ front-page.php
@@ -4,7 +4,10 @@
  *
  * @package     Scratch
  * @author      Javier Sánchez Serrano
+ * @link http://www.zumodigital.com
  * @copyright   Copyright (c) 2019 Javier Sánchez Serrano
  * @license     GPL-2.0+
  *
  */
+
+ genesis();
--- functions.php
+++ functions.php
@@ -9,4 +9,14 @@
  *
  */
 
- 
+ //Load chidl theme textdomain.
+ load_child_theme_textdomain('scratch');
+
+/**
+ * Theme setup.
+ * Attach all of the site-wide functions to the correct hooks and filters. All the funcions themeselves are defined below this setup function.
+ */
+ add_action('genesis_setup', 'scratch_setup');
+ function scratch_setup() {
+
+ }
