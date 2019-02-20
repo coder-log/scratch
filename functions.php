6252dbc2e7e6aee4074a3d6f1fbfbdaee78c4fb3
Xavier <43478582+coder-log@users.noreply.github.com>
2019-02-20 06:43:07 +0100

    Adding theme support

--- functions.php
+++ functions.php
@@ -18,5 +18,30 @@
  */
  add_action('genesis_setup', 'scratch_setup');
  function scratch_setup() {
+     //Define constants
+     Define('CHILD_THEME_NAME','Scratch');
+     Define('CHILD_THEME_URL', 'http:/github.com/coder-log/scratch');
+     Define('CHILD_THEME_VERSION', '1.0.0');
+
+    //copy pasted from init.php in genesis framework
+    //Add theme support for html5
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+    //Add viewport meta tag for mobile browsers
+    add_theme_support( 'genesis-responsive-viewport' );
+    //Add theme support for accesibility
+    add_theme_support(
+        'genesis-accessibility',
+        array(
+            '404-page',
+            'drop-down-menu',
+            'headings',
+            'rems',
+            'search-form',
+            'skip-links',
+        )
+    );
+
+    //Add theme support for footer widgets
+    add_theme_support('genesis_footer_widgets',3);
 
  }
