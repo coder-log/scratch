49516baf3f87985c60cf76f236c3dc5c30ff347d
Xavier <43478582+coder-log@users.noreply.github.com>
2019-02-22 08:13:05 +0100

    Display widgets

--- front-page.php
+++ front-page.php
@@ -10,4 +10,31 @@
  *
  */
 
+ add_action('genesis-meta', 'scratch_home_page_setup');
+
+ /**
+  * Setup the homepage layout by conditionally loading sections when 
+  * widgets are active
+  * @since 1.0.0
+  */
+$home_sidebars = array(
+    'home_welcome' => is_active_sidebar( 'home-welcome'),
+    'call_to_action' => is_active_sidebar( 'call-to.-action'),
+
+);
+
+function scratch_home_page_setup() {
+    //Return early if no sidebars are active
+    if (!in_array(true, $home_sidebars)) {
+        return;
+    }
+
+    if( $home_sidebars['home-welcome']) {
+        add_action('genesis_after_header', 'scratch_add_home_welcome');
+    }
+    if( $home_sidebars['call-to-action']) {
+        add_action('genesis_after_header', 'scratch_add_call_to_action');
+    }
+
+}
  genesis();
